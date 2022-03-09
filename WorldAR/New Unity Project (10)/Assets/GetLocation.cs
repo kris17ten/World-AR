86d2913f6ff1a3dff3d7925c525cd167e6b0f34e
Kristen Rebello <kristen.rebello@yahoo.co.uk>
2022-03-09 20:20:07 +0000

    push project

---
+++ WorldAR/New Unity Project (10)/Assets/GetLocation.cs
@@ -0,0 +1,39 @@
+﻿using GoogleARCore;
+using Mapbox.Unity.Map;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetLocation : MonoBehaviour
+{
+    public Text msg;
+    public GameObject map;
+    public Camera cam;
+    public GameObject place;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Began)
+            {
+                //map.GetComponent<AbstractMap>().Options.locationOptions.latitudeLongitude;
+                //msg.text = touch.position.ToString();
+                Instantiate(place, place.transform, true);
+                msg.text = Input.location.lastData.latitude + ", " + Input.location.lastData.longitude;
+                msg.text += " " + cam.ScreenToWorldPoint(new Vector3(Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, cam.nearClipPlane)).ToString();
+            }
+            // Instantiate prefab at the hit pose.
+            Instantiate(place, place.transform.position, place.transform.rotation);
+            
+        }
+    }
+}
